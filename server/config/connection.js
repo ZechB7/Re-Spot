adc2ab1b87db9811060d3432a93cb366a6e26c65
HoldyClaus <holdenclaus0212@gmail.com>
2023-03-09 14:54:15 -0600

    changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts',
+  process.env.MONGODB_URI || 'mongodb+srv://Holden0212:goofyboy41632@cluster0.yyz89kv.mongodb.net/reSpot',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
