dbe194d9afbcfefce2484582ac6de2b4d8fd4c75
HoldyClaus <holdenclaus0212@gmail.com>
2023-03-09 15:04:40 -0600

    change4

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://Holden0212:goofyboy41632@cluster0.yyz89kv.mongodb.net/reSpot',
+  process.env.MONGODB_URI || '',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
