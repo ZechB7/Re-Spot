f3bd638619b9b9c23057b3dfd377197ce3c3365f
HoldyClaus <holdenclaus0212@gmail.com>
2023-03-10 14:34:10 -0600

    changes to the header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,40 +3,42 @@ import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
 
+import './Header.css'; // import the stylesheet
+
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className="bg-black mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">Review Spotify Playlist</h1>
+          <Link className="" to="/" style={{ color: 'green' }}>
+            <h1 className="m-0 text-green header-title">Spotify Playlist Review</h1>
           </Link>
-          <p className="m-0">Discuss your peers playlist.</p>
+          <p className="m-0 text-white header-subtitle">Discuss your peers playlist.</p>
         </div>
         <div>
-          {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {Auth.getProfile().data.username}'s profile
+        {Auth.loggedIn() ? (
+        <>
+        <Link className="btn btn-lg m-2" to="/me">
+          {Auth.getProfile().data.username}'s profile
+          </Link>
+          <button className="btn btn-lg m-2" onClick={logout}>
+            Logout
+            </button>
+            </>)
+             : (
+             <>
+             <Link className="btn btn-lg m-2" to="/login">
+              Login
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              <Link className="btn btn-lg m-2" to="/signup">
                 Signup
-              </Link>
-            </>
-          )}
+                </Link>
+                </>
+              )}
         </div>
       </div>
     </header>
