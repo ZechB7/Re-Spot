93a7c9553e5b97ecd9233ab4b7c7fc62c294e51f
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-03-13 11:51:36 -0700

    weeweewoowee

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -13,7 +13,7 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="w-100 mt-auto bg-dark p-4">
       <div className="container text-center mb-5">
         {location.pathname !== '/' && (
           <button
