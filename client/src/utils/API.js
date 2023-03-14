fd0eb9b6512321f35c8133d81700b9f0f1909bd5
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-03-14 08:21:53 -0700

    pulling

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,11 +6,11 @@ const search = async (spotUser) => {
         method: 'GET',
         url: 'https://spotify23.p.rapidapi.com/user_profile/',
         headers: {
-            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+            'X-RapidAPI-Key': 'a21d4a375fmshc9eb4bf64e760f8p1e751ejsna643d2c9225f',
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         },
         params: {
-            id: `${spotUser}`, playlistLimit: '10', artistLimit: '10'
+            id: `${spotUser}`, playlistLimit: '2', artistLimit: '2'
         },
     })
 }
