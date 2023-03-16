bc0e2971f5345760eed5f1d9ceaf2edf58609d90
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-03-16 07:33:17 -0700

    hiding

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 mongoose.connect(
 
-  process.env.MONGODB_URI || 'mongodb+srv://root:Ruby10879@cluster0.a6mxemc.mongodb.net/reSpot',
-  // process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plantrackerDB',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plantrackerDB',
 
   {
     useNewUrlParser: true,
